8a581008e1ed7c84542dc517682b668bb2376331
grahamsw <grahams@pipeline.com>
2015-12-10 06:31:01 +0000

    added file for reading data and filtering to relevant 2 days

---
+++ readData.R
@@ -0,0 +1,5 @@
+fn <- 'household_power_consumption.txt'
+df <- read.table(fn, header=TRUE, sep=';', na='?', 
+        colClasses=c('character','character','numeric','numeric','numeric','numeric', 'numeric','numeric','numeric'))
+
+sub <- df[df$Date == "1/2/2007" | df$Date == "2/2/2007",  ]
