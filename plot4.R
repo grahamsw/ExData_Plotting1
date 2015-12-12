e6e9b77496b02afc6ffa96ccc05742d1530d8be3
grahamsw <grahams@pipeline.com>
2015-12-12 04:05:07 +0000

    fixed label on 4th plot

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@ source('readData.R')
 
 png('plot4.png')
 par(mfrow=c(2,2))
-plot(sub$DateTime, sub$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)')
+plot(sub$DateTime, sub$Global_active_power, type='l', xlab='', ylab='Global Active Power')
 plot(sub$DateTime, sub$Voltage, type='l', xlab='datetime', ylab='Voltage')
 plot(sub$DateTime, sub$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering')
 lines(sub$DateTime, sub$Sub_metering_2, col='red')
